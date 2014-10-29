2d69028484883afe5a652215d0f32853e36aa1d4
Jee Hoon <yamoo9@naver.com>
2014-10-29 10:44:33 +0900

    그리드 토글 버튼 상단 위치 조정

--- DAY08-landing/js/venders/toggleGrid.js
+++ DAY08-landing/js/venders/toggleGrid.js
@@ -3,7 +3,7 @@
 	document.body.insertAdjacentHTML('afterbegin', '<i tabindex="0" id="toggle-grid-button" class="fa fa-reorder"></i><div class="overlay-grid"></div>');
 	var toggleBtn = document.querySelector('#toggle-grid-button');
 
-	toggleBtn.style.cssText = 'position: fixed; z-index: 10001; right: 10px; top: 10px; cursor: pointer; font-size: 24px; color: #fff; background: #0b1919; padding: 4px 6px; border-radius: 3px;';
+	toggleBtn.style.cssText = 'position: fixed; z-index: 10001; right: 10px; top: 15px; cursor: pointer; font-size: 24px; color: #fff; background: #0b1919; padding: 4px 6px; border-radius: 3px;';
 
 	toggleBtn.onclick = document.onkeydown = function(e) {
 		var key = e.keyCode || e.which;
