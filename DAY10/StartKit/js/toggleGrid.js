b632f7c417653af5d1a5ac01373c07fabafb21b2
Jee Hoon <yamoo9@naver.com>
2015-05-19 21:24:49 +0900

    header>logo 부분 코드 추가

--- DAY10/StartKit/js/toggleGrid.js
+++ DAY10/StartKit/js/toggleGrid.js
@@ -2,9 +2,11 @@
 	var page = document.querySelector('#page');
 	document.onkeydown = function(event) {
 		var key = event.keyCode;
-		console.log(event);
+		// console.log(event);
 		// 사용자가 입력한 키가 'G'키라면...
-		if (key === 71 && event.ctrlKey === true) {
+		// g === 71
+		// d === 68 (Windows, Chrome 사용자)
+		if (key === 68 && event.shiftKey === true) {
 			page.classList.toggle('grid');
 		}
 	}
